7e04ed77ca4f78ef1a4e16018f15b011ed5c2329
Alfan <138642451+Alfan14@users.noreply.github.com>
2023-10-13 06:13:04 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+#Masukkan identitasmu 
+line=("-----------------------")
+name=input('Nama :')
+birth_date=input('Tgl lahir:')
+adress=input('Alamat :')
+hobby=input('hobby :')
+
+print(line)
+print(name)
+print(adress)
+print(birth_date)
+print(hobby)
+
+
